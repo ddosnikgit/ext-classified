dd5f7f886b56e0a28a9efd4a8e3237e03702bc35
David Ratnikov <95426617+ddosnikgit@users.noreply.github.com>
2023-07-04 21:03:01 +0300

    fixes2

--- classified.c
+++ classified.c
@@ -22,6 +22,10 @@ PHP_FUNCTION(classified_encode){
     RETURN_STR(r);
 }
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_classified_encode, 0, 0, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
+ZEND_END_ARG_INFO()
+
 PHP_FUNCTION(classified_decode) {
     char *encodedStr = "";
     size_t encodedStr_len = 0;
@@ -30,25 +34,21 @@ PHP_FUNCTION(classified_decode) {
     ZEND_PARSE_PARAMETERS_START(0,1)
         Z_PARAM_OPTIONAL
 		Z_PARAM_STRING(encodedStr, encodedStr_len)
-    ZEND_PARESE_PARAMETERS_END();
+    ZEND_PARSE_PARAMETERS_END();
 
     r = strpprintf(0, "Your input: %s", encodedStr);
 
     RETURN_STR(r);
 }
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_classified_encode, 0, 0, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
-ZEND_END_ARG_INFO()
-
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_classified_decode, 0, 0, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, str, IS_STRING, 0, "\"\"")
 ZEND_END_ARG_INFO()
 
 static const zend_function_entry classified_functions[] = {
-	ZEND_FE(classified_encode, arginfo_classified_encode)
-	ZEND_FE(classified_decode, arginfo_classified_decode)
-	ZEND_FE_END
+	PHP_FE(classified_encode, arginfo_classified_encode)
+	PHP_FE(classified_decode, arginfo_classified_decode)
+	PHP_FE_END
 };
 
 zend_module_entry classified_module_entry = {
